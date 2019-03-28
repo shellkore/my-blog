cd71f3455b974f987c08be4609631a343da0f01b
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-29 02:37:42 +0530

    added template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def polls_list(request):
-	return render(request,'blog/polls_list.html',{})
+def post_list(request):
+	return render(request,'blog/post_list.html',{})
